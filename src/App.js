9abf39a13c8de4a464d5c690b4605cea2eb06d12
Ivan Timofeev <skat.set@gmail.com>
2018-12-23 23:16:01 +0300

    Добавил первую реализацию

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Calendar from "react-calendar";
+import "./App.css";
+import moment from "moment";
 
 class App extends Component {
+
+  s = moment("2018-12-25");
+
+  tileClassName = ({date, view}) => {
+      if (view === "month") {
+          const d = -this.s.diff(date, "day");
+          if (d < 0) {
+            return null;
+          } else {
+            switch (d % 8) {
+              case 0:
+              case 1:
+                return "App-morning";
+              case 2:
+                return "App-weekend";
+              case 3:
+              case 4:
+                return "App-night";
+              case 5:
+                return "App-sleep";
+              case 6:
+              case 7:
+                return "App-weekend";
+              default:
+                alert("Something bad((");
+            }
+          }
+      }
+      return null;
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <Calendar minDetail="decade" tileClassName={this.tileClassName} />
+        </div>
       </div>
     );
   }
