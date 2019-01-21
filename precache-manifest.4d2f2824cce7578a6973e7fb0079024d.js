8be42d6dde0006873d8f71fae0c8ae2a87ff328b
Ivan Timofeev <i.timofeev@redmadrobot.com>
2019-01-21 22:55:09 +0300

    Updates

---
+++ precache-manifest.4d2f2824cce7578a6973e7fb0079024d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61fb1072ae27962f00a0",
+    "url": "/work-calendar/static/js/runtime~main.61fb1072.js"
+  },
+  {
+    "revision": "fbcf136ced0886e06aea",
+    "url": "/work-calendar/static/js/main.fbcf136c.chunk.js"
+  },
+  {
+    "revision": "ee900dc5662aceb3a59d",
+    "url": "/work-calendar/static/js/1.ee900dc5.chunk.js"
+  },
+  {
+    "revision": "fbcf136ced0886e06aea",
+    "url": "/work-calendar/static/css/main.a121ef45.chunk.css"
+  },
+  {
+    "revision": "ee900dc5662aceb3a59d",
+    "url": "/work-calendar/static/css/1.7d790aaf.chunk.css"
+  },
+  {
+    "revision": "14666a055addb2fd08268f5b926d5db3",
+    "url": "/work-calendar/index.html"
+  }
+];
